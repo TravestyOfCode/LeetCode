96134f8ece91ddd7d956dfe5a6967f7c1031cbac
travis@travestyofcode.com <travis@travestyofcode.com>
2023-05-24 19:27:08 -0400

    Adds solution and tests.

---
+++ LeetCode.Tests/Problem1/SolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Problem1;
+using Xunit;
+
+namespace LeetCode.Tests.Problem1
+{
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new int[]{2,7,11,15}, 9, new int[] { 1, 0 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 2, 1 })]
+        [InlineData(new int[] { 3,3 }, 6, new int[] { 1, 0 })]
+        [InlineData(new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11, new int[] { 11, 5 })]
+        public void FindsCorrectIndexs(int[] values, int target, int[] expected)
+        {
+            Solution solution = new Solution();
+
+            Assert.Equal(expected, solution.TwoSum(values, target));
+        }
+    }
+}
