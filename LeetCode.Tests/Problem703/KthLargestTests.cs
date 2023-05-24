9716fc3693d06320c8caa07a8a537df9453aa24c
travis@travestyofcode.com <travis@travestyofcode.com>
2023-05-24 14:00:42 -0400

    Added unit tests for individual methods.

---
+++ LeetCode.Tests/Problem703/KthLargestTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Problem703;
+using Xunit;
+
+namespace LeetCode.Tests.Problem703
+{
+    public class KthLargestTests
+    {
+        private KthLargest _KthLargest;
+
+        public KthLargestTests()
+        {
+            _KthLargest = new KthLargest(3, new int[] { 4, 5, 8, 2 });
+        }
+
+        [Fact]
+        public void GeneratesCorrectResults()
+        {
+            Assert.Equal(4, _KthLargest.Add(3));
+            Assert.Equal(5, _KthLargest.Add(5));
+            Assert.Equal(5, _KthLargest.Add(10));
+            Assert.Equal(8, _KthLargest.Add(9));
+            Assert.Equal(8, _KthLargest.Add(4));
+        }
+    }
+}
---
+++ LeetCode.Tests/Problem703/MinIntHeapTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace LeetCode.Tests.Problem703
+{
+    public class MinIntHeapTests
+    {
+
+        [Fact]
+        public void AddReturnsCorrectKthWithPositiveNumbers()
+        {
+            var _queue = new KthLargest(3, new int[] { 4, 5, 8, 2 });
+
+            Assert.Equal(4, _queue.Add(3));
+            Assert.Equal(5, _queue.Add(5));
+            Assert.Equal(5, _queue.Add(10));
+            Assert.Equal(8, _queue.Add(9));
+            Assert.Equal(8, _queue.Add(4));
+        }
+
+        [Fact]
+        public void AddReturnsCorrectKthWithNegativeNumbers()
+        {
+            var _queueNeg = new KthLargest(1, new int[] { });
+
+            Assert.Equal(-3, _queueNeg.Add(-3));
+            Assert.Equal(-2, _queueNeg.Add(-2));
+            Assert.Equal(-2, _queueNeg.Add(-4));
+            Assert.Equal(0, _queueNeg.Add(0));
+            Assert.Equal(4, _queueNeg.Add(4));
+        }
+    }
+}
