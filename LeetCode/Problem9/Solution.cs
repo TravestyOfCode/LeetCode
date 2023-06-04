f46905e40a3bf367dd154ae043ee2b3dad0d4253
travis@travestyofcode.com <travis@travestyofcode.com>
2023-06-03 23:14:09 -0400

    Adds Problem 9 solution and tests.

---
+++ LeetCode/Problem9/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problem9
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            int original = x;
+            int reversed = 0;
+            while(x > 0)
+            {
+                reversed = reversed * 10 + (x % 10);
+                x = x / 10;
+            }
+
+            return original == reversed;
+        }
+    }
+}
