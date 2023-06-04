94f33afedde68f4a0b7e212c1faaa379cd4d4e6f
travis@travestyofcode.com <travis@travestyofcode.com>
2023-06-04 18:37:03 -0400

    Adds problem and tests along with first attempt.

---
+++ LeetCode.Tests/Problems10To19/Problem12SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Problems10To19.Problem12;
+
+namespace LeetCode.Tests.Problems10To19
+{
+    public class Problem12SolutionTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void FirstAttemptCorrectlyConvertsToRoman(int num, string expected)
+        {
+            // Arrange
+            FirstAttempt solution = new FirstAttempt();
+
+            // Act
+            string result = solution.IntToRoman(num);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]{ 3, "III" },
+                new object[]{58, "LVIII"},
+                new object[]{1994, "MCMXCIV"}
+            };
+    }
+}
---
+++ LeetCode/Problems10To19/Problem12/FirstAttempt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems10To19.Problem12
+{
+    public class FirstAttempt
+    {
+        public string IntToRoman(int num)
+        {
+            // 1 <= num <= 3999
+            var ones = Convert(num % 10, 'I', 'V', 'X');
+            num = num / 10;
+            var tens = Convert(num % 10, 'X', 'L', 'C');
+            num = num / 10;
+            var hunds = Convert(num % 10, 'C', 'D', 'M');
+            num = num / 10;
+            var thous = Convert(num % 10, 'M', ' ', ' ');
+
+            return string.Join(string.Empty, thous, hunds, tens, ones);
+            
+        }
+
+        public string Convert(int num, char singleChar, char fiveChar, char tenChar)
+        {
+            if(num <= 3)
+            {
+                return new string(singleChar, num);
+            }
+            if(num == 4)
+            {
+                return string.Join(string.Empty, singleChar, fiveChar);
+            }
+            if(num == 5)
+            {
+                return fiveChar.ToString();
+            }
+            if(num <=8)
+            {
+                return string.Join(string.Empty, fiveChar, new string(singleChar, num - 5));
+            }
+            if(num == 9)
+            {
+                return string.Join(string.Empty, singleChar, tenChar);
+            }
+
+            return string.Empty;
+        }
+    }
+}
