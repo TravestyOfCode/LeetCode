1b1926b60628bca708b86957e0559def91b361d8
travis@travestyofcode.com <travis@travestyofcode.com>
2023-06-05 15:15:47 -0400

    Adds problem 14 solution.

--- LeetCode/Problems10To19/Problem14/FirstAttempt.cs
+++ LeetCode/Problems10To19/Problem14/FirstAttempt.cs
@@ -8,6 +8,26 @@ namespace LeetCode.Problems10To19.Problem14
 {
     public class FirstAttempt
     {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            // Find the longest common prefix amongst an array of strings
+            // If no common prefix, return an empty string.
+            if (strs.Length == 0)
+                return string.Empty;
 
+            string result = string.Empty;
+            for(int index = 0; index < strs[0].Length; index++)
+            {
+                char c = strs[0][index];
+                for(int subStrs = 1; subStrs < strs.Length; subStrs++)
+                {
+                    if (strs[subStrs].Length <= index ||  strs[subStrs][index] != c)
+                        return result;
+                }
+                result += c;
+            }            
+
+            return result;
+        }
     }
 }
