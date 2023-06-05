86dc70d5de6394171a850e944e72cf4746f620f0
travis@travestyofcode.com <travis@travestyofcode.com>
2023-06-05 14:56:52 -0400

    Adds Problem 14 and tests.

---
+++ LeetCode.Tests/Problems10To19/Problem14SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Problems10To19.Problem14;
+
+namespace LeetCode.Tests.Problems10To19
+{
+    public class Problem14SolutionTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void FirstAttemptCorrectlyFindsLongestCommonPrefix(string[] strs, string expected)
+        {
+            // Arrange
+            FirstAttempt solution = new FirstAttempt();
+
+            // Act
+            string result = solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]{ new string[] { "flower", "flow", "flight" }, "fl" },
+                new object[]{ new string[] { "dog", "racecar", "car" }, string.Empty }
+            };
+    }
+}
